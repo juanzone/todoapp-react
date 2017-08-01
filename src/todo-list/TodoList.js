061c583f8fdf30c0e683c1213236172745862e52
Juan Zone <zonexjuan@gmail.com>
2017-08-01 23:01:58 +0200

    integration redux v3

--- src/todo-list/TodoList.js
+++ src/todo-list/TodoList.js
@@ -9,23 +9,6 @@ class TodoList extends Component {
     super(props);
     this.getCheck = this.getCheck.bind(this);
     this.getTask = this.getTask.bind(this);
-
-    // this.state = {
-    //   taskList : [
-    //     {
-    //       name : 'Ranger chambre',
-    //       creator: 'Juan Zone',
-    //       date : new Date(2017,11,17).toDateString(),
-    //       check : false
-    //     },
-    //     {
-    //       name : 'Ranger Grenier',
-    //       creator: 'Philippe Zone',
-    //       date : new Date(2017,1,17).toDateString(),
-    //       check : false,
-    //     }
-    //   ]
-    // };
   }
 
   getTask(newtask){
